06b2efb49331d2f9fd875a21a878ab103641e0c6
William Marsman <william.marsman@gmail.com>
2019-12-10 13:32:00 -0400

    [jenkins-integration] it test for fully parameterized version of version uprevver

--- src/auto-version.spec.groovy
+++ src/auto-version.spec.groovy
@@ -8,6 +8,9 @@ def autoVersion = shell.parse(new File('src/auto-version.groovy'))
 
 // Release, file and version we're updating for
 // Hardcoded as an example here
+def projectParentDir=".."
+def projectDirName="auto-version-target"
+def projectGitURL="https://github.com/WilliamTheMarsman/auto-version-target.git"
 def release="release-0.1.0"
 def image="example-project"
 def version="0.1.0-rc4"
@@ -28,3 +31,15 @@ version="0.1.0-rc4"
 
 // Call the parameterized method
 autoVersion.autoVersion(release,image,version);
+
+////
+// Test to see the fully-parameterized version works
+////
+projectParentDir="/Users/wmarsman/work/test-alt"
+projectDirName="auto-version-target-test"
+projectGitURL="https://github.com/WilliamTheMarsman/auto-version-target.git"
+release="release-0.1.0"
+image="example-project"
+version="0.1.0-rc7"
+
+autoVersion.autoVersion(projectParentDir,projectDirName,projectGitURL,release,image,version)
